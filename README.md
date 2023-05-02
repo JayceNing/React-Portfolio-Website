2e8fe478205a4326253ccca9bd07c3cf864a00b9
NingXinyu <nxyqdl@163.com>
2023-05-02 16:53:12 +0800

    final version

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # React Portfolio Website
 
-YouTube 前端项目：[React Project Tutorial: Build a Responsive Portfolio Website w/ Advanced Animations (2022)](https://www.youtube.com/watch?v=hYv6BM2fWd8)
+YouTube 前端项目 学习笔记：[React Project Tutorial: Build a Responsive Portfolio Website w/ Advanced Animations (2022)](https://www.youtube.com/watch?v=hYv6BM2fWd8)
 
-学习笔记
+
+## 效果展示
 
 ![](./img/Home.png)
+
+![](./img/Home2.png)
+
+![](./img/Home3.png)
--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar} from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Footer } from "./components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
